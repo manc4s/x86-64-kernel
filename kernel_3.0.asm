dac4c48da1f4f7a48f4e8d6531aff8b8576d8485
manc4s <136273943+manc4s@users.noreply.github.com>
2025-06-06 03:48:35 -0400

    save_char, called on every print_char and is saved into a byte of 2112 bytes for 2112 possible chars at page_data in memory. also hovering which grabs the glyph from data and puts it into hovering char
    next steps:input buffers and moving the cursor around and then the base interface is done, and then parsing and commands can start beign implemented.
    
     -input buffer
     -move position on input for line
     -limit the input size
     - change input after moving cursor back into input (optional for now)

--- kernel_3.0.asm
+++ kernel_3.0.asm
@@ -16,105 +16,31 @@ start:
     mov ax, 0xA000
     mov es, ax
 
-
     ;draw the bg, so you can see the 320x200 screen in qemu, turns to selected bg_color
     call clear_screen
-
     
-     
+    ;entered kernel message
+    mov si, entered
+    call print_string
+    call new_line
+
+
     jmp main
 
 
 
 
-
-
-;clears the screen with selected color in al
-clear_screen:
-    ;preserve di
-    push di
-
-    mov di, 0                ; Start at offset 0 (row 0)
-    mov cx, 320*200              ; 320 pixels in the first row
-    mov al, [bg_color]    ;selected bg color in data, byte long        ; Color 00011100 (28),
-
-
-.fill_row:
-    mov [es:di], al
-    inc di
-    loop .fill_row
-
-    pop di
-    ret
-
-
-
-
-
-
-
-
-;Delay that is million loop cycles for cool typing effect
-big_delay:
-
-    ;200 * 65535 = 13,107,000 loop cycle delay
-    push bx
-    push cx
-
-    mov bx, 2500     ; outer loop count (increase for longer delay)
-
-.outer:
-    mov cx, 0FFFFh    ; inner loop count (max for cx)
-    ;decrement from 0xFFFF = 655535
-
-.inner:
-    loop .inner
-    dec bx
-    jnz .outer
-
-    pop cx
-    pop bx
-    ret
-
-
 main:
-    mov ax, 66
-    call print_char
-
-    ;mov byte [bg_color], 0x44
-    
-    mov word [x_offset], 100
-    mov word [y_offset], 100
-
+ 
     mov si, shell_line
     call print_string
 
-    call new_line
     
-    mov byte [bg_color], 0x04
-
-    mov al, 65
-    call print_char
-
-
-
-    ;where inputs should go
-    mov word [x_offset], 5
-    mov word [y_offset], 0
-    
-
-
-
-
 .main_loop:
 
 
 
     call toggle_cursor_color
-    
-    mov word [x_offset], 100
-    mov word [y_offset], 120
-
     call draw_cursor
 
     mov ah, 1
@@ -125,50 +51,76 @@ main:
     mov ax, 0x10                      ;the input value ascii goes in al
     int 0x16
 
-
-    push ax
-    mov word ax, [cursor_x]
-    mov word [x_offset], ax
-    mov word ax, [cursor_y]
-    mov word [y_offset], ax
-    pop ax
-
     
+   
+    cmp byte ah, 0x1C  ; enter pressed
+    je .enter_press
+
+    cmp byte al, 65
+    je .next
+    
+    ;else
     call print_char
-    add word [cursor_x], 5
-    
-    
-
+    call next_char
     jmp .main_loop
 
 
 
+.enter_press:
+    call draw_blank
+    call new_line
+ 
 
-toggle_cursor_color:
+    jmp main
+
+
+
+.next:
+
+    
+    mov word [x_offset], 10
+    mov word [y_offset], 120
+
+    call toggle_cursor_color_moved_pace
+    call draw_cursor
+
+
+    jmp .next
+
+
+
+
+
+
+toggle_cursor_color_moved_pace:
     push si
     push dx
     push ax
 
     inc word [cursor_counter]
+    cmp word [cursor_counter], 15000    ;the cursor blink rate 10000 ~= to 1 sec 
 
-
-    mov ax, [cursor_counter]
-    mov dx, 0
-    mov cx, 20000  ;100ticks
-    div cx  ;ax/cx remainder in dx
-    cmp dx, 0
-    je .change_to_bg_color
+    ;to avoid skipping, do when greater than 1000 than when equal.
+    jg .change_to_bg_color
     jmp .end
 
 .change_to_bg_color:    
    
-    cmp byte [cursor_bg], 0x57
+    ;reset the counter
+    mov word [cursor_counter], 0
+    
+    ;cursor color 1
+    mov byte al, [black]
+    cmp byte [cursor_bg], al
     je .to_black
-    mov byte [cursor_bg], 0x57
+    mov byte [cursor_bg], al
     jmp .end
 
 .to_black:
-    mov byte [cursor_bg], 0x00
+    
+    ;cursor_color2
+    mov al, [yella]
+    mov byte [cursor_bg], al
 
 .end:
     pop ax
@@ -179,13 +131,53 @@ toggle_cursor_color:
 
 
 
-draw_cursor:
+toggle_cursor_color:
+    push si
+    push dx
+    push ax
 
+    inc word [cursor_counter]
+    cmp word [cursor_counter], 5000    ;the cursor blink rate 10000 ~= to 1 sec 
+
+    ;to avoid skipping, do when greater than 1000 than when equal.
+    jg .change_to_bg_color
+    jmp .end
+
+.change_to_bg_color:    
+   
+    ;reset the counter
+    mov word [cursor_counter], 0
+    
+    ;cursor color 1
+    mov byte al, [black]
+    cmp byte [cursor_bg], al
+    je .to_black
+    mov byte [cursor_bg], al
+    jmp .end
+
+.to_black:
+    
+    ;cursor_color2
+    mov al, [yella]
+    mov byte [cursor_bg], al
+
+.end:
+    pop ax
+    pop dx
+    pop si
+    
+    ret
+
+
+
+
+draw_blank:
+    
     ;preserve ax
     push ax
-    mov al, [cursor_bg]
+    mov al, [olive]
     mov [bg_color], al
-    mov al, [cursor_text]
+    mov al, [olive]
     mov [text_color], al
     mov al, 32
     call print_char
@@ -196,6 +188,32 @@ draw_cursor:
     mov [text_color], al
     pop ax
     ret
+
+
+draw_cursor:
+
+    ;preserve ax
+    push ax
+    mov al, [cursor_bg]
+    mov [bg_color], al
+    mov al, [cursor_text]
+    mov [text_color], al
+
+    ;call scan_glyph_at_cursor
+    call hovering
+    mov al, [hovering_char]   
+    ;mov al, [page_data + 1] ; change to [hovering_char] after hovering the page_data
+    call print_char
+    
+    mov al, [olive]
+    mov [bg_color], al
+    mov al, [white]
+    mov [text_color], al
+    pop ax
+    ret
+
+
+
 
 
 print_string:
@@ -231,6 +249,106 @@ print_string:
     ret
 
 
+
+;grab value at [x_offset], [y_offset], put it into hovering_chat byte the asci value from page_data
+hovering:
+    push ax
+    push di
+    push dx
+    push bx
+    push si
+    push ds
+
+
+     ;x/5
+    push ax
+    mov ax, [x_offset]
+    mov dx, 0
+    mov bx, 5
+    div bx
+    mov si, ax
+    pop ax
+
+    ;y/6 * 64
+    push ax
+    mov ax, [y_offset]
+    mov dx, 0
+    mov bx, 6
+    div bx
+    imul ax, 64
+    add si, ax
+    pop ax
+
+
+    ;si should be offset for page_data now for entered al
+    mov byte al, [page_data + si]
+    mov byte [hovering_char], al
+    ;al is stores its ascii character in the 2112 bytes of characters
+
+
+    
+    pop ds
+    pop si
+    pop bx
+    pop dx
+    pop di
+    pop ax
+
+    ret
+
+
+
+
+;x and y taken, divided by 5 and 6 respectively. Values are added, y value * 64, for ascii location
+;page data 33*64 chars are stroed into 2112 bytes in memory at page_data
+;value hovered at x_offset y_offset saved to hovering_char
+save_char:
+
+    push ax
+    push di    
+    push dx
+    push bx
+    push si
+    push ds
+
+    ;x/5
+    push ax
+    mov ax, [x_offset]
+    mov dx, 0
+    mov bx, 5
+    div bx
+    mov si, ax
+    pop ax
+
+    ;y/6 * 64
+    push ax
+    mov ax, [y_offset]
+    mov dx, 0
+    mov bx, 6
+    div bx
+    imul ax, 64
+    add si, ax
+    pop ax
+
+
+    ;si should be offset for page_data now for entered al
+    mov byte [page_data + si], al
+    ;al is stores its ascii character in the 2112 bytes of characters
+
+
+    pop ds
+    pop si
+    pop bx
+    pop dx
+    pop di
+    pop ax
+
+    ret
+
+
+
+
+;prints the ascii byte in al
 print_char:
     push si
     push bx
@@ -239,6 +357,9 @@ print_char:
     mov di, [x_offset] ; [x_offset] works great for printing char by offset as well
     
     
+
+    call save_char  ;saves the char into page_data in its character of 2112 spots
+
     ;preserve ax here just in case
     push ax
     mov ax, [y_offset]
@@ -257,7 +378,7 @@ print_char:
     cmp bx, 6        ;loop 0-5
     jl .rest_of_print_char
 
-
+    
     pop bx
     pop si
     ret
@@ -271,12 +392,17 @@ new_line:
     add word [y_offset], 6
     mov word [x_offset], 0
 
-    cmp word [y_offset], 198
-    jge .row_end
+    ;33 rows startinf including 0
+    cmp word [row], 32
+    
+    je .row_end
+    inc word [row]
     ret
 
 .row_end:
+
     call clear_screen
+    mov word [row], 0
     mov word [y_offset], 0
     mov word [x_offset], 0
     ret
@@ -284,15 +410,16 @@ new_line:
 
 
 
+
+
 next_char:
     add word [x_offset], 5    ; should increment by 5
     cmp word [x_offset], 320
-    je .next
+    jge .next
     ret
 
 .next:
-    mov word [x_offset], 0
-    add word [y_offset], 6  ;glyphs are 8 pixels tall. so increment by 6
+    call new_line
     ret
 
 
@@ -373,7 +500,23 @@ plot_row_glyph:
 
 
 
+;clears the screen with selected color in al
+clear_screen:
+    ;preserve di
+    push di
 
+    mov di, 0                ; Start at offset 0 (row 0)
+    mov cx, 320*200              ; 320 pixels in the first row
+    mov al, [bg_color]    ;selected bg color in data, byte long        ; Color 00011100 (28),
+
+
+.fill_row:
+    mov [es:di], al
+    inc di
+    loop .fill_row
+
+    pop di
+    ret
 
 
 
